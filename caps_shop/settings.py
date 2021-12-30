62891a8b1d6c5c9576b7f4506969f231aac0578f
Esenbek <75290400+EsenbekM@users.noreply.github.com>
2021-12-30 19:42:15 +0600

    Update settings.py

--- caps_shop/settings.py
+++ caps_shop/settings.py
@@ -29,7 +29,7 @@ REST_FRAMEWORK = {
 SECRET_KEY = 'django-insecure-a^kce@urmy@ey5uawx2v7hx*x!$^$%o-h7pve5kh!8)d=%i$1w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
