6aef5813ef197a219834d71dffec59ae833874f4
Esen <esenbekm03@gmail.com>
2021-12-30 19:21:53 +0600

    get ready for push

--- caps_shop/urls.py
+++ caps_shop/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from main import views as product_view
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,3 +10,7 @@ urlpatterns = [
     path('api/v1/products/', product_view.ProductListAPIView.as_view()),
     path('api/v1/products/<int:id>/', product_view.ProductDetailAPIView.as_view()),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
