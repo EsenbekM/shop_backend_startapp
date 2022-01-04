1883cb066324d176373980b90feb2bf4d8a94391
Esen <esenbekm03@gmail.com>
2022-01-04 17:35:41 +0600

    deploy

---
+++ caps_shop/local_settings.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+import os
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = 'django-insecure-a^kce@urmy@ey5uawx2v7hx*x!$^$%o-h7pve5kh!8)d=%i$1w'
+
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'shop_backend_db',
+        'USER': 'admin',
+        'PASSWORD': 'esenbek123',
+        'HOST':'localhost',
+        'PORT': '5432'
+    }
+}
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
---
+++ caps_shop/prod_settings.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+import os
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = 'django-insecure-a^kce@urmy@ey5uawx2v7hx*x!$^$%o-h7pve5kh!8)d=%i$1w'
+
+DEBUG = False
+
+ALLOWED_HOSTS = ["127.0.0.1"]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'shop_backend_db',
+        'USER': 'admin',
+        'PASSWORD': '123456',
+        'HOST':'localhost',
+        'PORT': '5432'
+    }
+}
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+try:
+    from .local_settings import *
+except ImportError:
+    from .prod_settings import *
